dfe658115865c0a0dc7b6486702bda1cc495b4c9
vedrakholia32 <vedrakholia32@gmail.com>
2024-05-06 22:02:07 +0530

    Final

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,38 +1,49 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    console.log("Uppercase was clicked " + text);
-    let newText = text.toLocaleUpperCase();
+    let newText = text.toUpperCase();
     setText(newText);
   };
 
   const handleLoClick = () => {
-    console.log("Uppercase was clicked " + text);
-    let newText = text.toLocaleLowerCase();
+    let newText = text.toLowerCase();
     setText(newText);
   };
 
   const handleClearText = () => {
-    let newText = ''
-    setText(newText)
-  }
+    setText("");
+  };
 
   const handleOnChange = (event) => {
     setText(event.target.value);
-    console.log(text);
   };
 
   const [text, setText] = useState("");
+
+  // Function to calculate word count
+  const calculateWordCount = (inputText) => {
+    const trimmedText = inputText.trim();
+    const words = trimmedText.split(/\s+/);
+    const filteredWords = words.filter((word) => word !== "");
+    return filteredWords.length;
+  };
 
   return (
     <>
       <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='light'?'white':'grey', color:props.mode==='dark'?'white':'black'}}  onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"
-            
+          <textarea
+            className="form-control"
+            value={text}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "grey",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            onChange={handleOnChange}
+            id="exampleFormControlTextarea1"
+            rows="8"
           ></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>
@@ -50,9 +61,12 @@ export default function TextForm(props) {
       <div className="container my-3">
         <h1>Your Text Summary</h1>
         <p>
-          {text.split(" ").length} words and {text.length} characters
+          {calculateWordCount(text)}{" "}
+          {calculateWordCount(text) === 1 ? "word" : "words"} and {text.length}{" "}
+          characters
         </p>
-        <p>{0.008 * text.split(" ").length} Minutes Read </p>
+
+        <p>{0.008 * calculateWordCount(text)} Minutes Read </p>
         <h2 className="my-2">Preview</h2>
         <p>{text}</p>
       </div>
